a969bbfd415154116364df08fdd05ad13e4a7c49
doga ozcan <dogasava@gmail.com>
2018-12-20 22:36:20 +0300

    Update privileges.sol

--- contracts/privileges.sol
+++ contracts/privileges.sol
@@ -1,5 +1,4 @@
 pragma solidity ^0.4.24;
-pragma experimental ABIEncoderV2;
 
 contract privileges {
 
@@ -8,22 +7,21 @@ contract privileges {
     }
     
     struct screenPrivileges {
-        string screen_code;
-        string starting_date;
-        string ending_date;
+        bytes32 screen_code;
+        bytes32 starting_date;
+        bytes32 ending_date;
     }
     
-    string[] screen_code_list;
-    string[] starting_date_list;
-    string[] ending_date_list;
+    bytes32[]  screen_code_list;
+    bytes32[] starting_date_list;
+    bytes32[] ending_date_list;
     mapping(address => privilege) privilege_list;
     mapping(address => uint) screenCountSize;
     
-    function update(uint newSize,address privilegeOwner,string[] memory screenCode,string[] memory startingDate,string[] memory endingDate) public returns (bool){
+    function update(uint newSize,string __privilegeOwner,bytes32[]  screenCode,bytes32[]  startingDate,bytes32[]  endingDate) public returns (bool){
+        address privilegeOwner = parseAddr(__privilegeOwner);
         delete privilege_list[privilegeOwner]._screenPrivileges;
-        delete screen_code_list;
-        delete starting_date_list;
-        delete ending_date_list; 
+     
         
         for(uint i=0;i<newSize;i++){
             screenPrivileges memory localdata = screenPrivileges(screenCode[i],startingDate[i],endingDate[i]);
@@ -32,7 +30,8 @@ contract privileges {
         screenCountSize[privilegeOwner] = newSize;
         return true;
     }
-    function get(address privilegeOwner) public returns (string[] memory screenCode,string[] memory startingDate,string[] memory endingDate){
+    function get(string __privilegeOwner) public returns (bytes32[]  screenCode,bytes32[]   startingDate,bytes32[]   endingDate){
+        address privilegeOwner = parseAddr(__privilegeOwner);
         for(uint i=0;i<privilege_list[privilegeOwner]._screenPrivileges.length;i++){
               screen_code_list.push(privilege_list[privilegeOwner]._screenPrivileges[i].screen_code);
               starting_date_list.push(privilege_list[privilegeOwner]._screenPrivileges[i].starting_date);
@@ -40,4 +39,23 @@ contract privileges {
         }
         return (screen_code_list,starting_date_list,ending_date_list);
     }
+    
+    function parseAddr(string _a) internal returns (address){
+        bytes memory tmp = bytes(_a);
+        uint160 iaddr = 0;
+        uint160 b1;
+        uint160 b2;
+        for (uint i=2; i<2+2*20; i+=2){
+            iaddr *= 256;
+            b1 = uint160(tmp[i]);
+            b2 = uint160(tmp[i+1]);
+            if ((b1 >= 97)&&(b1 <= 102)) b1 -= 87;
+            else if ((b1 >= 48)&&(b1 <= 57)) b1 -= 48;
+            if ((b2 >= 97)&&(b2 <= 102)) b2 -= 87;
+            else if ((b2 >= 48)&&(b2 <= 57)) b2 -= 48;
+            iaddr += (b1*16+b2);
+        }
+        return address(iaddr);
+    }
+
 }
