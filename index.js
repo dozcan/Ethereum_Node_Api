b77f1a220f4b1ac9617d4e96c516ba929aa08a65
doga ozcan <dogasava@gmail.com>
2020-10-21 17:04:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 
 var requestUrl = "http://18.236.226.143:8545";
 // default rpc port 8545, ikinci container portuna dönüştürülebilinir.
-var web3 = new Web3(new Web3.providers.HttpProvider(requestUrl));
+var web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/v3/6e32e89d0e2349748b38e1ca7aa46a7c"));
 var express = require('express');
 const app = express();
 var bodyParser = require('body-parser');
@@ -129,38 +129,42 @@ const DeployContract = async (provider, interface, bytecode, account) => {
             "address":"deploy edilen contract adresi"
         }
 *output: blockchain verisinin hash değeri ve blockchaine atılan transaciton hash değeri*/
-app.post('/Identity', function (req, res) {
+app.post('/yeniTarla', function (req, res) {
 
     var set = async () => {
         try {
-
-            let blockchainDataName = JSON.stringify(req.body.name);
-            let blockchainDataSurname = JSON.stringify(req.body.surname);
-            let blockchainDataIdentity = JSON.stringify(req.body.identity);
-            let contractAddressFromClient = JSON.stringify(req.body.address);
-            contractAddressFromClient = helper.cleanWhiteCharacter(contractAddressFromClient);
-            blockchainDataName = helper.cleanWhiteCharacter(blockchainDataName);
-            blockchainDataSurname = helper.cleanWhiteCharacter(blockchainDataSurname);
-            blockchainDataIdentity = helper.cleanWhiteCharacter(blockchainDataIdentity);
-            var hashedData = helper.sha256Hash(blockchainDataIdentity, blockchainDataName, blockchainDataSurname);
-
-            bytes = [];
-
+            let enlem = JSON.stringify(req.body.enlem);
+            let boylam = JSON.stringify(req.body.boylam);
+            let urunTipi = JSON.stringify(req.body.urunTipi);
+            let sahipTckn = JSON.stringify(req.body.KisiTckn);
+            let sahipAd = JSON.stringify(req.body.KisiAd);
+            let sahipSoyad = JSON.stringify(req.body.KisiSoyad);
+            
+            enlem = helper.cleanWhiteCharacter(enlem);
+            boylam = helper.cleanWhiteCharacter(boylam);
+            urunTipi = helper.cleanWhiteCharacter(urunTipi);
+            sahipTckn = helper.cleanWhiteCharacter(sahipTckn);
+            sahipAd = helper.cleanWhiteCharacter(sahipAd);
+            sahipSoyad = helper.cleanWhiteCharacter(sahipSoyad);
             accounts = await web3.eth.getAccounts();
             if (contractInstance === undefined || contractInstance === null) {
                 contractInstance = await new web3.eth.Contract(JSON.parse(interface), contractAddressFromClient);
             }
-            var data = helper.byteConversion(blockchainDataIdentity);
             try {
 
-                await contractInstance.methods.setIdentity(hashedData, blockchainDataName, blockchainDataSurname, data).
+                await contractInstance.methods.setNewTarla(
+                    Web3.utils.stringToHex(enlem),
+                    Web3.utils.stringToHex(boylam),
+                    Web3.utils.stringToHex(urunTipi),
+                    Web3.utils.stringToHex(sahipTckn),
+                    Web3.utils.stringToHex(sahipAd),
+                    Web3.utils.stringToHex(sahipSoyad)
+                ).
                     send({
                         from: accounts[0],
                         gas: '100000000'
                     }, function (err, result) {
                         if (!err) {
-
-                            console.log(result);
                             hashTransactionOfSetMethod = result;
                         }
                         else {
@@ -174,12 +178,8 @@ app.post('/Identity', function (req, res) {
 
 
 
-                key = ["account", "data_hash", "transaction_hash"];
-                console.log("identity:");
-                console.log(data);
-                console.log("hash");
-                console.log(hashedData);
-                value = [accounts[0], hashedData, hashTransactionOfSetMethod];
+                key = ["account", "yeni tarla", "tarla işlem id"];
+                value = [accounts[0], "yaratıldı", hashTransactionOfSetMethod];
                 rawResponseObject = responseMaker.createResponse(key, value);
                 response = responseMaker.responseMaker(rawResponseObject);
                 res.send(response);
@@ -202,6 +202,81 @@ app.post('/Identity', function (req, res) {
     set();
 });
 
+
+app.post('/yeniPolice ', function (req, res) {
+
+    var set = async () => {
+        try {
+            let acente = JSON.stringify(req.body.acente);
+            let sigortaBaslangic = JSON.stringify(req.body.sigortaBaslangic);
+            let sigortaBitis = JSON.stringify(req.body.sigortaBitis);
+            let sigortaTur = JSON.stringify(req.body.sigortaTur);
+            let sigortaTutar = JSON.stringify(req.body.sigortaTutar);
+            let tckn = JSON.stringify(req.body.tckn);
+            
+            acente = helper.cleanWhiteCharacter(acente);
+            sigortaBaslangic = helper.cleanWhiteCharacter(sigortaBaslangic);
+            sigortaBitis = helper.cleanWhiteCharacter(sigortaBitis);
+            sigortaTur = helper.cleanWhiteCharacter(sigortaTur);
+            sigortaTutar = helper.cleanWhiteCharacter(sigortaTutar);
+            tckn = helper.cleanWhiteCharacter(tckn);
+            accounts = await web3.eth.getAccounts();
+            if (contractInstance === undefined || contractInstance === null) {
+                contractInstance = await new web3.eth.Contract(JSON.parse(interface), contractAddressFromClient);
+            }
+            try {
+
+                await contractInstance.methods.setNewPolice(
+                    Web3.utils.stringToHex(acente),
+                    Web3.utils.stringToHex(sigortaBaslangic),
+                    Web3.utils.stringToHex(sigortaBitis),
+                    Web3.utils.stringToHex(sigortaTur),
+                    Web3.utils.stringToHex(sigortaTutar),
+                    Web3.utils.stringToHex(tckn)
+                ).
+                    send({
+                        from: accounts[0],
+                        gas: '100000000'
+                    }, function (err, result) {
+                        if (!err) {
+                            hashTransactionOfSetMethod = result;
+                        }
+                        else {
+                            hashTransactionOfSetMethod = "";
+                            errorCode = requestTypeError.identity;
+                            errorMessage = helper.error(errorCode, err);
+                            response = responseMaker.responseErrorMaker(errorCode, errorMessage);
+                            res.send(response);
+                        }
+                    });
+
+
+
+                key = ["account", "yeni police", "police tekil id"];
+                value = [accounts[0], "yaratıldı", hashTransactionOfSetMethod];
+                rawResponseObject = responseMaker.createResponse(key, value);
+                response = responseMaker.responseMaker(rawResponseObject);
+                res.send(response);
+            }
+            catch (err) {
+                errorCode = requestTypeError.identity_transactional_hash;
+                errorMessage = helper.error(errorCode, err);
+                response = responseMaker.responseErrorMaker(errorCode, errorMessage);
+                res.send(response);
+            }
+
+        }
+        catch (err) {
+            errorCode = requestTypeError.identity;
+            errorMessage = helper.error(errorCode, err);
+            response = responseMaker.responseErrorMaker(errorCode, errorMessage);
+            res.send(response);
+        }
+    }
+    set();
+});
+
+
 /*Veriyi blockchain'den almak için rest api url
 *Çağırım : http://ip:port/HashGet
 *input :{
@@ -209,42 +284,33 @@ app.post('/Identity', function (req, res) {
             "address":"deploy edilen contract adresi"
         }
 *output: blockchain orjinal veriyi geri döndürür*/
-app.post('/HashGet', function (req, res) {
+app.get('/yeniTarla', function (req, res) {
 
     var get = async () => {
 
         try {
-            let contractAddressFromClient = JSON.stringify(req.body.address);
-            let hashofBlockchainData = JSON.stringify(req.body.hashofBlockchainData);
-            contractAddressFromClient = helper.cleanWhiteCharacter(contractAddressFromClient);
-            hashofBlockchainData = helper.cleanWhiteCharacter(hashofBlockchainData);
+         
+            let tarlaId = JSON.stringify(req.body.tarlaId);
+            tarlaId = helper.cleanWhiteCharacter(tarlaId);
 
             accounts = await web3.eth.getAccounts();
 
             if (contractInstance === undefined || contractInstance === null) {
                 contractInstance = await new web3.eth.Contract(JSON.parse(interface), contractAddressFromClient);
             }
-            console.log(hashofBlockchainData);
-            console.log(accounts[0]);
 
-            var respondData = await getIdentity(contractInstance, accounts[0], hashofBlockchainData);
+            var respondData = await getCreatedTarla(contractInstance, accounts[0], tarlaId);
 
-            var name = respondData[0];
-            console.log("name:" + name);
-            var surname = respondData[1];
-            console.log("surname:" + surname);
-            var identity = respondData[2];
-            console.log("identity:" + identity);
-
+            var urunTipi = Web3.utils.hexToString(respondData[0]);
+            var sahipTckn = Web3.utils.hexToString(respondData[1]);
 
             var responseRaw = {
-                name: name,
-                surname: surname,
-                identity: identity
+                urunTipi: urunTipi,
+                sahipTckn: sahipTckn
             };
 
-            key = ["data", "data_hash"];
-            value = [responseRaw, hashofBlockchainData];
+            key = ["tarla bilgileri", "tarla işlem id"];
+            value = [responseRaw, tarlaId];
             rawResponseObject = responseMaker.createResponse(key, value);
             response = responseMaker.responseMaker(rawResponseObject);
             res.send(response);
@@ -260,10 +326,59 @@ app.post('/HashGet', function (req, res) {
     get();
 });
 
+app.get('/yeniPolice', function (req, res) {
 
-const getIdentity = async (contractClone, account, client) => {
+    var get = async () => {
+
+        try {
+         
+            let policeId = JSON.stringify(req.body.policeId);
+            policeId = helper.cleanWhiteCharacter(policeId);
+
+            accounts = await web3.eth.getAccounts();
+
+            if (contractInstance === undefined || contractInstance === null) {
+                contractInstance = await new web3.eth.Contract(JSON.parse(interface), contractAddressFromClient);
+            }
+
+            var respondData = await getCreatedPolice(contractInstance, accounts[0], policeId);
+
+            var acente = Web3.utils.hexToString(respondData[0]);
+            var sigortaBaslangic = Web3.utils.hexToString(respondData[1]);
+            var sigortaBitis = Web3.utils.hexToString(respondData[2]);
+            var sigortaTur = Web3.utils.hexToString(respondData[3]);
+            var sigortaTutar = Web3.utils.hexToString(respondData[4]);
+            var tckn = Web3.utils.hexToString(respondData[5]);
+
+            var responseRaw = {
+                acente: acente,
+                sigortaBaslangic: sigortaBaslangic,
+                sigortaBitis:sigortaBitis,
+                sigortaTur:sigortaTur,
+                sigortaTutar:sigortaTutar,
+                tckn:tckn
+            };
+
+            key = ["poliçe bilgileri", "poliçe işlem id"];
+            value = [responseRaw, policeId];
+            rawResponseObject = responseMaker.createResponse(key, value);
+            response = responseMaker.responseMaker(rawResponseObject);
+            res.send(response);
+
+        }
+        catch (err) {
+            errorCode = requestTypeError.hash_get;
+            errorMessage = helper.error(errorCode, err);
+            response = responseMaker.responseErrorMaker(errorCode, errorMessage);
+            res.send(response);
+        }
+    }
+    get();
+});
+
+const getCreatedTarla = async (contractClone, account, tarlaId) => {
     try {
-        var data = await contractClone.methods.get(client).call({ from: account })
+        var data = await contractClone.methods.getCreatedTarla(tarlaId).call({ from: account })
         return data;
     }
     catch (err) {
@@ -271,10 +386,22 @@ const getIdentity = async (contractClone, account, client) => {
     }
 }
 
+const getCreatedPolice = async (contractClone, account, policeId) => {
+    try {
+        var data = await contractClone.methods.getCreatedPolice(policeId).call({ from: account })
+        return data;
+    }
+    catch (err) {
+        throw (err);
+    }
+}
+
+
 module.exports = {
     AccountCreate,
     DeployContract,
-    getIdentity
+    getCreatedTarla,
+    getCreatedPolice
 }
 
 app.listen(6000, () => {
