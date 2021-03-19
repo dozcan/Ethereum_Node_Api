70392e36ff70d28b4df1ff65b450fbe3c1b9f002
doga ozcan <dogasava@gmail.com>
2021-03-19 23:13:04 +0300

    Update urunTarlaContract.sol

--- contracts/urunTarlaContract.sol
+++ contracts/urunTarlaContract.sol
@@ -1,34 +1,60 @@
-pragma solidity >=0.2.0 <0.8.0;
+pragma solidity >=0.4.0 <0.8.0;
 
-contract urunTarlaContract {
+contract rol {
     
-    struct tarla {
-        bytes enlem;
-        bytes boylam;
-        bytes urunTipi;
-        bytes sahipTckn;
-        bytes sahipAd;
-        bytes sahipSoyad;
+    struct adres
+    {
+        bytes _adres;
+        lokasyon _lokasyon;
     }
     
-    mapping(uint => tarla) tarlalar;
-    uint tarlaIdCount=1;
     
-    function setNewTarla(bytes memory _enlem,
-        bytes memory _boylam,
-        bytes memory  _urunTipi,
-        bytes memory _sahipTckn,
-        bytes memory _sahipAd,
-        bytes memory _sahipSoyad)public returns (uint createdTarlaId){
-         
-        createdTarlaId =  tarlaIdCount++;
-        tarlalar[createdTarlaId] = tarla(_enlem,_boylam,_urunTipi,_sahipTckn,_sahipAd,_sahipSoyad);
+    struct lokasyon {
+        uint x;
+        uint y;
     }
+
     
-    function getCreatedTarla(uint createdTarlaId) public view returns(bytes memory  _urunTipi,
-        bytes memory _sahipTckn){
-            
-            return (tarlalar[createdTarlaId].urunTipi,tarlalar[createdTarlaId].sahipTckn);
+    mapping (address => adres) adresler;
+    
+    address [] adrescikler;
+    
+    function setAddres(address adr,bytes memory _adres, uint x,uint y) public returns (bool){
+        
+        require(adr == msg.sender,"adres sahibi guncelleme yapabilir");
+        
+        if(adresler[adr]._adres.length == 0){ //ekleme
+              adresler[msg.sender] = adres(_adres,lokasyon(x,y));
+              adrescikler.push(adr);
         }
-}
+        else{
+            adres storage val = adresler[msg.sender];
+            val._lokasyon.x = x;
+            val._lokasyon.y = y;
+        }
+        
+      
+        return true;
+        
+    }
+
     
+    function getAddress(address _adres) public view returns (bytes memory,uint,uint) {
+        
+        require(_adres == msg.sender,"adres sahibi veriyi cekebilir");
+        
+        return (adresler[msg.sender]._adres,adresler[msg.sender]._lokasyon.x,adresler[msg.sender]._lokasyon.y);
+        
+    
+    }
+    
+    function getaddsler()public view returns (address [] memory){
+        return adrescikler;
+        
+    }
+    
+    
+    
+    
+    
+}
